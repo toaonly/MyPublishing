d233816362ac0dc1467dde015f68ab902809244f
toaonly <toaonly42@gmail.com>
2018-09-11 20:26:37 +0900

    First commit, gulp settings and 09/01/2018 publishing complete

---
+++ README.md
@@ -0,0 +1,19 @@
+# My publishing projects
+---
+
+## 셋팅
+
+`gulp`를 사용하여 아래 파일들을 처리
+
+* CSS
+	* SASS(SCSS)
+	* LESS
+	* Stylus
+* JavaScript
+	* Babel
+		* ES6
+		* ES7
+		* ES8
+		* ESNext
+* Watch
+	* 파일 변경 자동 감지하여 변환
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const less = require('gulp-less');
+const stylus = require('gulp-stylus');
+const serverIO = require('gulp-server-io');
+
+const PATH = {
+  HTML: 'src/**/*.html',
+  JS: 'src/**/*.js',
+  LESS: 'src/**/*.less',
+  SCSS: 'src/**/*.scss',
+  SASS: 'src/**/*.sass',
+  STYLUS: 'src/**/*.stylus'
+};
+
+gulp.task('babel', () => {
+  gulp.src(PATH.JS)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat(`chunk${(new Date).getTime()}.js`))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', () => {
+  gulp.src(PATH.LESS)
+    .pipe(less())
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('sass', () => {
+  gulp.src([ PATH.SASS, PATH.SCSS ])
+    .pipe(sass().on('error', function(err) {
+      console.warn(`[SASS-ERROR] ${err.file}`);
+      console.warn(`[LINE] line: ${err.line}, column ${err.column}`);
+      console.warn(`[MESSAGE] ${err.message}`);
+      this.emit('end');
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('stylus', () => {
+  gulp.src(PATH.STYLUS)
+    .pipe(stylus())
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', () => {
+  gulp.watch(PATH.JS, [ 'babel' ]);
+  gulp.watch(PATH.LESS, [ 'less' ]);
+  gulp.watch([ PATH.SASS, PATH.SCSS ], [ 'sass' ]);
+  gulp.watch(PATH.STYLUS, [ 'stylus' ]);
+});
+
+gulp.task('server', () => {
+  return gulp.src([ 'src/index.html', PATH.HTML ])
+    .pipe(serverIO({
+      port: 8080,
+      webroot: 'src',
+      serverReload: {
+        dir: 'src/index.html',
+        config: { debounce: 1000 }
+      }
+    }));
+});
+
+gulp.task('default', [ 'babel', 'less', 'sass', 'stylus', 'watch' ])
